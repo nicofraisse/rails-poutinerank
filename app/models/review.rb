c44f05f5c6ea32a6f58abb86a01834e0d0321034
Nicolas Fraisse <ni.fraisse@gmail.com>
2020-05-27 07:40:08 -0400

    fixed global rating validation

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,7 +3,7 @@ class Review < ApplicationRecord
   belongs_to :user
   belongs_to :poutine_category
   validates :user_id, presence: true
-  validates :global_rating, presence: true, :inclusion => 1..5
+  validates :global_rating, presence: true, :inclusion => 1..10
   validates :title, presence: true, length: { minimum: 4, maximum: 80 }
   validates :body, presence: true, length: { minimum: 20, maximum: 5000 }
   after_initialize :init
