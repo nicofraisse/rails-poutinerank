953d9fa73c64fa8c33347a4b1a1edd54346ff5c0
Nicolas Fraisse <ni.fraisse@gmail.com>
2020-05-27 05:26:07 -0400

    have to commit

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -24,41 +24,40 @@ document.addEventListener('turbolinks:load', () => {
 
 // For the searchable/sortable table
 $(document).ready(function () {
-$('#table-index').DataTable({
-  "lengthChange": false,
-  "dom": '<"text-left ml-3 searchbar-index"f>',
-  "oLanguage": {
-     "sSearch": "Filtrer"
-   }
-
-});
-$('.dataTables_length').addClass('bs-select');
-
+  $('#table-index').DataTable({
+    "lengthChange": false,
+    "dom": '<"text-left ml-3 searchbar-index"f>',
+    "oLanguage": {
+       "sSearch": "Filtrer"
+     }
+  });
+  $('.dataTables_length').addClass('bs-select');
 });
 
 
 // For the homepager carousel
-var numberOfCarousels = $('.carousel').length
-for (let i = 0; i < numberOfCarousels; i++) {
-  $(`#carousel-example-${i}`).on(`slide.bs.carousel`, function (e) {
-    var $e = $(e.relatedTarget);
-    var idx = $e.index();
-    var itemsPerSlide = 4;
-    var totalItems = this.querySelectorAll('.carousel-item').length;
-    console.log(idx, itemsPerSlide, totalItems)
-    if (idx >= totalItems - (itemsPerSlide - 1)) {
-      var it = itemsPerSlide - (totalItems - idx);
-      for (var j=0; j< it ; j++) {
-        // append slides to end
-        if (e.direction == "left") {
-          $(`.ci-${i}`).eq(j).appendTo(`.c-${i}`);
-        }
-        else {
-          $(`.ci-${i}`).eq(0).appendTo(`.c-${i}`);
+$(document).ready(function () {
+  var numberOfCarousels = $('.carousel').length
+  for (let i = 0; i < numberOfCarousels; i++) {
+    $(`#carousel-example-${i}`).on(`slide.bs.carousel`, function (e) {
+      var $e = $(e.relatedTarget);
+      var idx = $e.index();
+      var itemsPerSlide = 4;
+      var totalItems = this.querySelectorAll('.carousel-item').length;
+      console.log(idx, itemsPerSlide, totalItems)
+      if (idx >= totalItems - (itemsPerSlide - 1)) {
+        var it = itemsPerSlide - (totalItems - idx);
+        for (var j=0; j< it ; j++) {
+          // append slides to end
+          if (e.direction == "left") {
+            $(`.ci-${i}`).eq(j).appendTo(`.c-${i}`);
+          }
+          else {
+            $(`.ci-${i}`).eq(0).appendTo(`.c-${i}`);
+          }
         }
       }
-    }
-  });
-}
-
+    });
+  }
+});
 
