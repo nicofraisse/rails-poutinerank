a2c23792ec0e01f3f40405cf9b2533f27a088e41
Nicolas Fraisse <ni.fraisse@gmail.com>
2020-05-28 18:09:39 -0400

    skip authentification on restaurants index show

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,6 +1,6 @@
 class RestaurantsController < ApplicationController
   skip_after_action :verify_policy_scoped, :only => [:index, :show]
-
+  skip_before_action :authenticate_user!, :only => [:index, :show]
   def index
     if params[:query].present?
       @restaurants = Restaurant.global_search(params[:query])
