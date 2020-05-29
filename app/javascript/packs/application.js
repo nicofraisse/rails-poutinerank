68b5b7a6cdb0c151964bc241dbc26817a05b4024
Nicolas Fraisse <ni.fraisse@gmail.com>
2020-05-29 17:20:24 -0400

    added more/less button for revealing text

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -82,3 +82,27 @@ $(document).on('turbolinks:load', function () {
 });
 
 
+// For showing less/more words
+$(document).on('turbolinks:load', function () {
+  $.fn.clicktoggle = function(a, b) {
+      return this.each(function() {
+          var clicked = false;
+          $(this).click(function() {
+              if (clicked) {
+                  clicked = false;
+                  return b.apply(this, arguments);
+              }
+              clicked = true;
+              return a.apply(this, arguments);
+          });
+      });
+  };
+
+  $(".more").clicktoggle(function() {
+    $(this).text("less..").siblings(".complete").show();
+  }, function() {
+    $(this).text("more..").siblings(".complete").hide();
+  });
+});
+
+
